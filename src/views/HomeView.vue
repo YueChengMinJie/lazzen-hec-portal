b2fff780a149557f062793b23976ae5a5cef3745
caszhou <zhouxiajie@qq.com>
2025-05-09 16:42:43 +0800

    chore: eslint fix.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,16 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  const handleBtnClick = () => {
+    console.log('zxj', 1111);
+  };
+</script>
 
 <template>
-  <div class="text-red-500 dark:text-red-400"> TailwindCSS Test </div>
+  <div>
+    <div class="text-red-500 dark:text-red-400"> TailwindCSS Test </div>
+    <div>
+      <a-button type="primary" @click="handleBtnClick">Primary Button</a-button>
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
